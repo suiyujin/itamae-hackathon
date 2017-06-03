a5fe63487f01a2d8805a84e5a3c70e17bacacb9b
suiyujin <suiyujin04@gmail.com>
2017-06-03 16:45:48 +0900

    not link if file exists

--- cookbooks/tmux/default.rb
+++ cookbooks/tmux/default.rb
@@ -1,5 +1,8 @@
 include_recipe 'tmux'
 
 execute 'link so file' do
-  command 'ln -s /usr/local/lib/libevent-2.1.so.6 /usr/lib64/libevent-2.1.so.6'
+  from_file = '/usr/local/lib/libevent-2.1.so.6'
+  to_file = '/usr/lib64/libevent-2.1.so.6'
+  command "ln -s #{from_file} #{to_file}"
+  not_if "test -e #{to_file}"
 end
