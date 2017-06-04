ab2ffaf31d0f9a426f6d193e969828e144a9adca
suiyujin <suiyujin04@gmail.com>
2017-06-04 15:05:10 +0900

    install mysql

--- cookbooks/base/default.rb
+++ cookbooks/base/default.rb
@@ -1,5 +1,8 @@
+package "epel-release"
+
 include_recipe '../user/default.rb'
 include_recipe 'rtn_git::system'
 include_recipe '../tmux/default.rb'
 include_recipe '../zsh/default.rb'
 include_recipe '../nginx/default.rb'
+include_recipe '../mysql/default.rb'
